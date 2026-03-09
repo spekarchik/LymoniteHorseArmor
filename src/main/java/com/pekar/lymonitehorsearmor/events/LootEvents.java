7df4136d5cb45d040112424f3ef82b3334428db9
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-03-09 02:10:24 +0100

    Loot chance adjusted

--- src/main/java/com/pekar/lymonitehorsearmor/events/LootEvents.java
+++ src/main/java/com/pekar/lymonitehorsearmor/events/LootEvents.java
@@ -1,6 +1,7 @@
 package com.pekar.lymonitehorsearmor.events;
 
 import com.pekar.lymonitehorsearmor.armor.ArmorRegistry;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.entries.EmptyLootItem;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
@@ -13,16 +14,11 @@ public class LootEvents implements IEventHandler
     @SubscribeEvent
     public void onChestLootTableLoad(LootTableLoadEvent event)
     {
-        chestLootTableLoad(event);
-    }
-
-    private void chestLootTableLoad(LootTableLoadEvent event)
-    {
-        if (event.getName().toString().equals("minecraft:chests/jungle_temple"))
+        if (event.getKey().equals(BuiltInLootTables.JUNGLE_TEMPLE))
         {
             var pool = LootPool.lootPool()
                     .add(LootItem.lootTableItem(ArmorRegistry.HORSE_LYMONITE_ARMOR).setWeight(1))
-                    .add(EmptyLootItem.emptyItem().setWeight(4))
+                    .add(EmptyLootItem.emptyItem().setWeight(1))
                     .setRolls(ConstantValue.exactly(1f))
                     .build();
 
@@ -30,11 +26,11 @@ public class LootEvents implements IEventHandler
             return;
         }
 
-        if (event.getName().toString().equals("minecraft:chests/woodland_mansion"))
+        if (event.getKey().equals(BuiltInLootTables.WOODLAND_MANSION))
         {
             var pool = LootPool.lootPool()
                     .add(LootItem.lootTableItem(ArmorRegistry.HORSE_LYMONITE_ARMOR).setWeight(1))
-                    .add(EmptyLootItem.emptyItem().setWeight(2))
+                    .add(EmptyLootItem.emptyItem().setWeight(1))
                     .setRolls(ConstantValue.exactly(1f))
                     .build();
 
