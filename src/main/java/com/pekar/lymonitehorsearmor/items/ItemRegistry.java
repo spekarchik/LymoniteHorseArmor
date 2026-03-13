894c1391e61a98554e47762c17e1ef72f07ff12c
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-03-13 01:50:03 +0100

    Horse Armor Handbook added

--- src/main/java/com/pekar/lymonitehorsearmor/items/ItemRegistry.java
+++ src/main/java/com/pekar/lymonitehorsearmor/items/ItemRegistry.java
@@ -8,6 +8,7 @@ import net.neoforged.neoforge.registries.DeferredItem;
 public class ItemRegistry
 {
     public static final DeferredItem<Item> LYMONITE_INGOT = Main.ITEMS.registerItem("lymonite_ingot", LymoniteIngot::new, p -> p.rarity(Rarity.UNCOMMON));
+    public static final DeferredItem<Item> HORSE_ARMOR_HANDBOOK = Main.ITEMS.registerItem("horse_armor_handbook", AnimalArmorHandbook::new, p -> p.rarity(Rarity.UNCOMMON).stacksTo(4));
 
     public static void initStatic()
     {
