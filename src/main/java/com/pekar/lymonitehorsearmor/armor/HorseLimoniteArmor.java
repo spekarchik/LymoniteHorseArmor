d19749d9193b0c2e158f2a0596b930bf9eccfe48
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-03-13 01:09:55 +0100

    Updated horse armor: rarity and tooltip

--- src/main/java/com/pekar/lymonitehorsearmor/armor/HorseLimoniteArmor.java
+++ src/main/java/com/pekar/lymonitehorsearmor/armor/HorseLimoniteArmor.java
@@ -2,13 +2,14 @@ package com.pekar.lymonitehorsearmor.armor;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.equipment.ArmorType;
 
 public class HorseLimoniteArmor extends ModHorseArmor
 {
     protected HorseLimoniteArmor(ModArmorMaterial material, ArmorType equipmentSlot, Properties properties)
     {
-        super(material, equipmentSlot, AnimalArmorType.LYMONITE_HORSE, properties);
+        super(material, equipmentSlot, AnimalArmorType.LYMONITE_HORSE, properties.rarity(Rarity.UNCOMMON));
     }
 
     @Override
