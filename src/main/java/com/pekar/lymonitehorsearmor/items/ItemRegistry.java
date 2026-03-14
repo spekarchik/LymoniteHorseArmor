64be31adc42c57bd34f1c6faae99da4ce612f5b6
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-03-14 03:17:11 +0100

    Downgraded to MC 1.21.9

--- src/main/java/com/pekar/lymonitehorsearmor/items/ItemRegistry.java
+++ src/main/java/com/pekar/lymonitehorsearmor/items/ItemRegistry.java
@@ -9,7 +9,7 @@ public class ItemRegistry
 {
     public static final DeferredItem<Item> LYMONITE_MIXTURE = Main.ITEMS.registerItem("lymonite_mixture", ModItem::new);
     public static final DeferredItem<Item> LYMONITE_INGOT = Main.ITEMS.registerItem("lymonite_ingot", LymoniteIngot::new);
-    public static final DeferredItem<Item> HORSE_ARMOR_HANDBOOK = Main.ITEMS.registerItem("horse_armor_handbook", AnimalArmorHandbook::new, p -> p.rarity(Rarity.UNCOMMON).stacksTo(4));
+    public static final DeferredItem<Item> HORSE_ARMOR_HANDBOOK = Main.ITEMS.registerItem("horse_armor_handbook", AnimalArmorHandbook::new);
 
     public static void initStatic()
     {
