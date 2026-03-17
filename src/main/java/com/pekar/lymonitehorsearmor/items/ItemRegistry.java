0fce4ea4425606993ac18c48f859d207bf445e16
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-03-17 03:52:46 +0100

    Lymonite ingot: removed tooltip about loot

--- src/main/java/com/pekar/lymonitehorsearmor/items/ItemRegistry.java
+++ src/main/java/com/pekar/lymonitehorsearmor/items/ItemRegistry.java
@@ -7,7 +7,7 @@ import net.neoforged.neoforge.registries.DeferredItem;
 public class ItemRegistry
 {
     public static final DeferredItem<Item> LYMONITE_MIXTURE = Main.ITEMS.registerItem("lymonite_mixture", ModItem::new);
-    public static final DeferredItem<Item> LYMONITE_INGOT = Main.ITEMS.registerItem("lymonite_ingot", LymoniteIngot::new);
+    public static final DeferredItem<Item> LYMONITE_INGOT = Main.ITEMS.registerItem("lymonite_ingot", ModItem::new);
     public static final DeferredItem<Item> HORSE_ARMOR_HANDBOOK = Main.ITEMS.registerItem("horse_armor_handbook", AnimalArmorHandbook::new);
 
     public static void initStatic()
