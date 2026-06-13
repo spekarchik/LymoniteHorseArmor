89864c1ce8fb60f3dfe35b5cd77858b0b2494e01
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-06-14 01:58:47 +0200

    Fixed an issue where the Creative Mode tab title could display the name of another mod.
    (cherry picked from commit 932bf4fd2517e6fc37d1e3f85e67475904fdaaab)
    (cherry picked from commit b2a2f4c33748f3dc52441b35087f28d6ade5c72c)

--- src/main/java/com/pekar/lymonitehorsearmor/tab/ModTab.java
+++ src/main/java/com/pekar/lymonitehorsearmor/tab/ModTab.java
@@ -25,7 +25,7 @@ public abstract class ModTab
 
     protected String getTitle()
     {
-        return "itemGroup." + getTabName();
+        return "itemGroup." + MODID + "." + getTabName();
     }
 
     public final CreativeModeTab createTab()
